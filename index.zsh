6a5e3a6dcf34c9a16a9cfe6827b55e74865256a3
Maxime Laforet <maxime10081985@gmail.com>
2014-10-20 22:38:07 +0200

    More noticeable echos

--- index.zsh
+++ index.zsh
@@ -13,11 +13,13 @@ eval COLOR_RESET='$reset_color'
 # add some helpers for display
 export CHAR_OK=✔
 export CHAR_ERROR=✗
+export CHAR_WARNING=⚠️
 export CHAR_BOOM=💥
 export CHAR_STARTER=❯
 function echo_info () { echo "${COLOR_BLUE}i${COLOR_RESET} $1" }
 function echo_user () { echo "${COLOR_YELLOW}?${COLOR_RESET} $1" }
 function echo_success () { echo "${COLOR_GREEN}${CHAR_OK}${COLOR_RESET} $1" }
+function echo_warning () { echo "${CHAR_WARNING}  $1"}
 function echo_fail () { echo "${COLOR_RED}${CHAR_ERROR}${COLOR_RESET} $1|n"; exit }
 function echo_title () { echo "${COLOR_CYAN}${CHAR_STARTER} $@${COLOR_RESET}" }
 function echo_title_install () { echo_title "Installing" $1 "..." }
