f7ebfb8715fc53e3fabcbbe75469e15c1be33a25
Maxime Laforet <me@macx.im>
2017-04-17 23:10:51 +0100

    Add wallpaper component

---
+++ components/wallpaper/aliases.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+# Change background via reddit-background
+alias change-bckgd="/usr/local/bin/reddit-backgrounds/reddit-background"
+
+# Open reddit-background documentation
+alias reddit-bckgd-doc="e /usr/local/bin/reddit-backgrounds/README.md"
